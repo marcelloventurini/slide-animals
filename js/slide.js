f0046c43b2a98e77c84d7cb567f6a8ff7e23e8db
Marcello Venturini <marcellofventurini@gmail.com>
2023-03-30 14:41:17 -0300

    update distances.

--- js/slide.js
+++ js/slide.js
@@ -2,18 +2,38 @@ export default class Slide {
   constructor(slide, container) {
     this.slide = document.querySelector(slide)
     this.container = document.querySelector(container)
+    this.distance = {
+      finalPosition: 0,
+      startX: 0,
+      movement: 0
+    }
+  }
+
+  moveSlide(distX) {
+    this.distance.movePosition = distX
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`
+  }
+
+  updatePosition(clientX) {
+    this.distance.movement = (this.distance.startX - clientX) * 1.5
+    return this.distance.finalPosition - this.distance.movement
   }
 
   onStart(event) {
     event.preventDefault()
+
+    this.distance.startX = event.clientX
     this.container.addEventListener('mousemove', this.onMove)
   }
 
   onMove(event) {
+    const finalPosition = this.updatePosition(event.clientX)
+    this.moveSlide(finalPosition)
   }
 
   onEnd(event) {
     this.container.removeEventListener('mousemove', this.onMove)
+    this.distance.finalPosition = this.distance.movePosition
   }
 
   addSlideEvents() {
